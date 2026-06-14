984defc1f68f71be859f272cb497bf6320963b0b
elakkiya010607 <aloneponnuelakki007@gmail.com>
2026-06-14 13:56:47 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,19 @@
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    operator = input("Enter +, -, *, /: ")
+    if operator == "+":
+        print(num1 + num2)
+    elif operator == "-":
+        print(num1 - num2)
+    elif operator == "*":
+        print(num1 * num2)
+    elif operator == "/":
+        if num2 == 0:
+            print("Cannot divide by zero!")
+        else:
+            print(num1 / num2)
+    else:
+        print("Invalid operator!")
+except ValueError:
+    print("Please enter numbers only!")
